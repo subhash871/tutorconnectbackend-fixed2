fbe54f7b05ba26b5d9b85dcb9b36f33f3c98f021
subhash <ratusubhash2002@gamil.com>
2026-07-07 17:59:24 +0545

    Fix Celery to use REDIS_URL instead of hard

--- config/settings.py
+++ config/settings.py
@@ -309,8 +309,8 @@ CACHES = (
 )
 
 # Celery Configuration
-CELERY_BROKER_URL = config('CELERY_BROKER_URL', default='redis://localhost:6379/0')
-CELERY_RESULT_BACKEND = config('CELERY_RESULT_BACKEND', default='redis://localhost:6379/0')
+CELERY_BROKER_URL = config('CELERY_BROKER_URL', default=REDIS_URL)
+CELERY_RESULT_BACKEND = config('CELERY_RESULT_BACKEND', default=REDIS_URL)
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
