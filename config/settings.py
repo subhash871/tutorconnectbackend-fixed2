c2120ea3ae811b9c16c2acc8033c3217255db4aa
subhash <ratusubhash2002@gamil.com>
2026-07-07 18:31:37 +0545

    hfh

--- config/settings.py
+++ config/settings.py
@@ -339,6 +339,7 @@ EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_TIMEOUT = 10  # seconds; fail fast instead of hanging on bad SMTP config
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='noreply@tutorconnectnepal.com')
 FRONTEND_URL = config('FRONTEND_URL', default='http://localhost:3000')
 
