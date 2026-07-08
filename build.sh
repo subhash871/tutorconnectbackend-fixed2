2ee11dd2627a72d1ffd4ac5295ec9ccf6b95786f
subhash <ratusubhash2002@gamil.com>
2026-07-08 07:58:58 +0545

    Remove one-time superuser reset script

--- build.sh
+++ build.sh
@@ -4,22 +4,3 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-python manage.py shell << 'PYEOF'
-from apps.users.models import User
-
-email = "rautsubhash2002@gmail.com"
-password = "Subhash@2020"
-
-user, created = User.objects.get_or_create(email=email, defaults={
-    "is_staff": True,
-    "is_superuser": True,
-    "is_active": True,
-})
-user.is_staff = True
-user.is_superuser = True
-user.is_active = True
-user.set_password(password)
-user.save()
-print(f"Superuser ready: {email} (created={created})")
-PYEOF
